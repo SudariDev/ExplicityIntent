41a8f89263a7b7df81f03da7bdb26fb6687441ce
USER-PC <wasu19981107@gmail.com>
2021-02-18 16:57:25 +0530

    Initial commit

---
+++ app/src/main/java/com/example/explicityintent/Activity2.java
@@ -0,0 +1,32 @@
+package com.example.explicityintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity2 extends AppCompatActivity {
+
+    Button b2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+        b2 =findViewById(R.id.btn2);
+        
+        b2.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Activity2.this,MainActivity.class);
+                startActivity(intent);
+                
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/explicityintent/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.explicityintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = findViewById(R.id.btn1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to activity 2
+                Intent intent = new Intent(MainActivity.this,Activity2.class);
+                startActivity(intent);
+
+            }
+
+
+        });
+    }
+}
